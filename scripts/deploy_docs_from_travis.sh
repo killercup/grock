89d9c01d2de6958bf6bba13e50ad5d919782a03d
Pascal Hertleif <killercup@gmail.com>
2014-01-25 15:58:39 +0100

    Use --github for Travis Deploy
    Sadly, this stuff is all trial & error.

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-rm -rf docs || exit 0;
+git config user.name "Travis-CI"
+git config user.email "travis@pascalhertleif.de"
+git remote set-url origin "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
 
-CURRENT_COMMIT=`git rev-parse HEAD`
-
-./bin/grock --verbose --out docs/
-
-( cd docs
-  git init
-  git config user.name "Travis-CI"
-  git config user.email "travis@pascalhertleif.de"
-  git add .
-  git commit -m "Documentation for ${CURRENT_COMMIT}"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+./bin/grock --verbose --github > /dev/null 2>&1
