8deb7b56d80f6db5abc26d1131707b8fadf429b9
Pascal Hertleif <killercup@gmail.com>
2014-01-25 17:29:14 +0100

    Travis Deploy to gh-pages, take 4
    Yep, it's boring.

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -3,9 +3,6 @@ echo "Setting up git config"
 
 CURRENT_COMMIT=`git rev-parse HEAD`
 
-git config user.name "Travis-CI"
-git config user.email "travis@pascalhertleif.de"
-
 git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" docs > /dev/null 2>&1 || exit 1
 
 rm -r docs/*
@@ -16,6 +13,9 @@ echo "Compiling docs"
 cd docs/
 
 echo "Pushing to gh-pages"
+git config user.name "Travis-CI"
+git config user.email "travis@pascalhertleif.de"
+
 git add -A
 git commit -m "Generated documentation for $CURRENT_COMMIT" || exit 1
 git push origin gh-pages > /dev/null 2>&1 || exit 1
