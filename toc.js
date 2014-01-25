c2157dfec8b9ba3ec5b5d0118803f2eff7390d05
Travis-CI <travis@pascalhertleif.de>
2014-01-25 14:48:58 +0000

    Documentation for ea458fde422aeb33a3e4e512cc20ab918bf51ffa

---
+++ toc.js
@@ -0,0 +1,316 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "Readme.md",
+  "originalPath": "Readme.md",
+  "name": "index.html",
+  "title": "grock",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "grock",
+      "title": "grock"
+    },
+    {
+      "level": 2,
+      "slug": "install",
+      "title": "Install"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "inspiration",
+      "title": "Inspiration"
+    },
+    {
+      "level": 3,
+      "slug": "what-makes-grock-different-",
+      "title": "What makes grock different?"
+    },
+    {
+      "level": 2,
+      "slug": "based-on",
+      "title": "Based on"
+    },
+    {
+      "level": 2,
+      "slug": "roadmap",
+      "title": "Roadmap"
+    }
+  ]
+},
+{
+  "path": "lib/docTags.coffee.html",
+  "originalName": "docTags.coffee",
+  "originalPath": "lib/docTags.coffee",
+  "name": "docTags.coffee.html",
+  "lang": "coffeescript",
+  "title": "Known Doc Tags",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "known-doc-tags",
+      "title": "Known Doc Tags"
+    }
+  ]
+},
+{
+  "path": "lib/generator.coffee.html",
+  "originalName": "generator.coffee",
+  "originalPath": "lib/generator.coffee",
+  "name": "generator.coffee.html",
+  "lang": "coffeescript",
+  "title": "Documetation Generator",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "documetation-generator",
+      "title": "Documetation Generator"
+    },
+    {
+      "level": 2,
+      "slug": "npm-modules",
+      "title": "NPM Modules"
+    },
+    {
+      "level": 2,
+      "slug": "local-modules",
+      "title": "Local Modules"
+    },
+    {
+      "level": 2,
+      "slug": "helpers",
+      "title": "Helpers"
+    },
+    {
+      "level": 3,
+      "slug": "calculate-runtime",
+      "title": "Calculate Runtime"
+    },
+    {
+      "level": 2,
+      "slug": "the-glorious-generator",
+      "title": "The Glorious Generator"
+    },
+    {
+      "level": 3,
+      "slug": "processing-pipeline",
+      "title": "Processing Pipeline"
+    },
+    {
+      "level": 4,
+      "slug": "log-process-duration",
+      "title": "Log process duration"
+    },
+    {
+      "level": 3,
+      "slug": "process-style",
+      "title": "Process Style"
+    }
+  ]
+},
+{
+  "path": "lib/languages.coffee.html",
+  "originalName": "languages.coffee",
+  "originalPath": "lib/languages.coffee",
+  "name": "languages.coffee.html",
+  "lang": "coffeescript",
+  "title": "Supported Languages",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "supported-languages",
+      "title": "Supported Languages"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/getLanguage.coffee.html",
+  "originalName": "getLanguage.coffee",
+  "originalPath": "lib/transforms/getLanguage.coffee",
+  "name": "getLanguage.coffee.html",
+  "lang": "coffeescript",
+  "title": "Get File Language",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "get-file-language",
+      "title": "Get File Language"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/highlight.coffee.html",
+  "originalName": "highlight.coffee",
+  "originalPath": "lib/transforms/highlight.coffee",
+  "name": "highlight.coffee.html",
+  "lang": "coffeescript",
+  "title": "Highlight Code",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "highlight-code",
+      "title": "Highlight Code"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/index.coffee.html",
+  "originalName": "index.coffee",
+  "originalPath": "lib/transforms/index.coffee",
+  "name": "index.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "lib/transforms/indexFile.coffee.html",
+  "originalName": "indexFile.coffee",
+  "originalPath": "lib/transforms/indexFile.coffee",
+  "name": "indexFile.coffee.html",
+  "lang": "coffeescript",
+  "title": "Rename Index File",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "rename-index-file",
+      "title": "Rename Index File"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/markdownComments.coffee.html",
+  "originalName": "markdownComments.coffee",
+  "originalPath": "lib/transforms/markdownComments.coffee",
+  "name": "markdownComments.coffee.html",
+  "lang": "coffeescript",
+  "title": "Render Markdown Comments",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "render-markdown-comments",
+      "title": "Render Markdown Comments"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/renderDocTags.coffee.html",
+  "originalName": "renderDocTags.coffee",
+  "originalPath": "lib/transforms/renderDocTags.coffee",
+  "name": "renderDocTags.coffee.html",
+  "lang": "coffeescript",
+  "title": "Render Doc Tags to Markdown",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "render-doc-tags-to-markdown",
+      "title": "Render Doc Tags to Markdown"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/renderFileTree.coffee.html",
+  "originalName": "renderFileTree.coffee",
+  "originalPath": "lib/transforms/renderFileTree.coffee",
+  "name": "renderFileTree.coffee.html",
+  "lang": "coffeescript",
+  "title": "Write JSON File Tree",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "write-json-file-tree",
+      "title": "Write JSON File Tree"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/renderTemplates.coffee.html",
+  "originalName": "renderTemplates.coffee",
+  "originalPath": "lib/transforms/renderTemplates.coffee",
+  "name": "renderTemplates.coffee.html",
+  "lang": "coffeescript",
+  "title": "Render Templates",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "render-templates",
+      "title": "Render Templates"
+    },
+    {
+      "level": 2,
+      "slug": "variables-accessable-in-template",
+      "title": "Variables accessable in template"
+    }
+  ]
+},
+{
+  "path": "lib/transforms/splitCodeAndComments.coffee.html",
+  "originalName": "splitCodeAndComments.coffee",
+  "originalPath": "lib/transforms/splitCodeAndComments.coffee",
+  "name": "splitCodeAndComments.coffee.html",
+  "lang": "coffeescript",
+  "title": "Split Code and Comments",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "split-code-and-comments",
+      "title": "Split Code and Comments"
+    }
+  ]
+},
+{
+  "path": "lib/utils/humanize.coffee.html",
+  "originalName": "humanize.coffee",
+  "originalPath": "lib/utils/humanize.coffee",
+  "name": "humanize.coffee.html",
+  "lang": "coffeescript",
+  "title": "Humanize Strings",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "humanize-strings",
+      "title": "Humanize Strings"
+    }
+  ]
+},
+{
+  "path": "lib/utils/log.coffee.html",
+  "originalName": "log.coffee",
+  "originalPath": "lib/utils/log.coffee",
+  "name": "log.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "lib/utils/processDocTags.coffee.html",
+  "originalName": "processDocTags.coffee",
+  "originalPath": "lib/utils/processDocTags.coffee",
+  "name": "processDocTags.coffee.html",
+  "lang": "coffeescript",
+  "title": "Process Doc Tags",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "process-doc-tags",
+      "title": "Process Doc Tags"
+    }
+  ]
+},
+{
+  "path": "lib/utils/regexpEscape.coffee.html",
+  "originalName": "regexpEscape.coffee",
+  "originalPath": "lib/utils/regexpEscape.coffee",
+  "name": "regexpEscape.coffee.html",
+  "lang": "coffeescript",
+  "title": "Regex Escape",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "regex-escape",
+      "title": "Regex Escape"
+    }
+  ]
+}
