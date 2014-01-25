d4dcfc5faa34664ef166d5f64d73fc18930a31ac
Pascal Hertleif <killercup@gmail.com>
2014-01-25 17:25:58 +0100

    Travis Deploy to gh-pages, take 3
    Sadly, this stuff is all trial & error.

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 echo "Setting up git config"
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+
 git config user.name "Travis-CI"
 git config user.email "travis@pascalhertleif.de"
-git remote set-url origin "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
 
-echo "Compiling docs and pushing to gh-pages"
-./bin/grock --verbose --github > /dev/null 2>&1
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" docs > /dev/null 2>&1 || exit 1
+
+rm -r docs/*
+
+echo "Compiling docs"
+./bin/grock --verbose --out docs/ || exit 1
+
+cd docs/
+
+echo "Pushing to gh-pages"
+git add -A
+git commit -m "Generated documentation for $CURRENT_COMMIT" || exit 1
+git push origin gh-pages > /dev/null 2>&1 || exit 1
+
+echo "Pushed new documentation successfully."
+exit 0
