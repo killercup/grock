b37c82de0e8198fca29d9af7116e225716514b43
Pascal Hertleif <killercup@gmail.com>
2014-01-25 16:40:08 +0100

    Travis Deploy to gh-pages, take 2
    Sadly, this stuff is all trial & error.

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Setting up git config"
 git config user.name "Travis-CI"
 git config user.email "travis@pascalhertleif.de"
 git remote set-url origin "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
 
+echo "Compiling docs and pushing to gh-pages"
 ./bin/grock --verbose --github > /dev/null 2>&1
