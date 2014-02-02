e10dd51b341ef67a40fb01969fb9fecd7f8cd8fb
Pascal Hertleif <killercup@gmail.com>
2014-02-02 14:13:53 +0100

    Fix `/index.js`

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-require('coffee-script');
+require('coffee-script/register');
+
+var pkg = require('./package.json');
 
 module.exports = {
+  name: pkg.name,
+  version: pkg.version,
   generator: require('./lib/generator')
 };
